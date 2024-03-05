45fbf3c0c9e8cc00dedf771da37e1eebb8e785b1
omobolaji001 <omobolajiolayiwola11@gmail.com>
2024-03-05 18:35:55 +0100

    user.routes completed

--- tradehub-api/create_users.js
+++ tradehub-api/create_users.js
@@ -1,17 +1,22 @@
 const User = require('./models/user');
+const sequelize = require('./config/db');
 
 
-
-const getUser = (userId) => {
-  User.findOne({where: {id: userId}}).then((user) => {
-    if (!user) {
-      console.log("Not found");
-      return
-    }
-    console.log(user.toJSON());
-  }).catch((err) => {
-    console.error(err);
-  });
+updates = {id: 100, fullName: "Ifafunto Ibrahim James", email: "ifajames@gmail.com"};
+data = [];
+for (const attr in updates) {
+  data.push(attr);
 }
+console.log(data.length);
 
-getUser(10);
+User.findByPk(2).then((user) => {
+  for (const attr in updates) {
+    if (updates.hasOwnProperty(attr) && attr !== 'id') {
+      user[attr] = updates[attr];
+    }
+  }
+  user.save();
+  console.log(user.toJSON());
+}).catch((err) => {
+  console.error(err.errors[0].message);
+});
--- tradehub-api/routes/users.routes.js
+++ tradehub-api/routes/users.routes.js
@@ -1,7 +1,14 @@
 const router = require('express').Router();
-const { getUser, getUsers } = require('../controllers/users.controller.js')
+const { getUser, 
+	getUsers, 
+	createUser,
+        updateUser,
+	deleteUser } = require('../controllers/users.controller.js')
 
 router.get('/', getUsers);
 router.get('/:userId', getUser);
+router.post('/', createUser);
+router.put('/:userId', updateUser);
+router.delete('/:userId', deleteUser);
 
 module.exports = router
