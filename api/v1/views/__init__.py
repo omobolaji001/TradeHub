d57b5d9ca94f0ff10b2af3efad0ddbfcade93ca4
omobolaji001 <omobolajiolayiwola11@gmail.com>
2024-09-08 19:40:42 +0100

    Initialize blueprint in __init__.py

---
+++ api/v1/views/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" Blueprint for API """
+from flask import Blueprint
+
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
+from api.v1.views.users import *
+from api.v1.views.products import *
+from api.v1.views.orders import *
+from api.v1.views.carts import *
