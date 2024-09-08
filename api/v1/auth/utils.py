2dca8af09c9d45ba9487ba7ba4e75a7ccf2c0615
omobolaji001 <omobolajiolayiwola11@gmail.com>
2024-09-08 22:35:34 +0100

    Implemented authorize decorator to allow access for only specified users

--- api/v1/auth/utils.py
+++ api/v1/auth/utils.py
@@ -27,3 +27,30 @@ def token_required(f):
 
         return f(*args, **kwargs)
     return decorated
+
+
+def authorize(required_role: str):
+    """Decorator to check if the user has the required role."""
+    def decorator(f):
+        @wraps(f)
+        def decorated_function(*args, **kwargs):
+            token = request.headers.get('Authorization')
+            if not token:
+                return jsonify({'message': 'Token is missing!'}), 401
+
+            try:
+                token = token.split(" ")[1]  # 'Bearer token'
+                payload = auth.verify_jwt(token)
+                if payload is None:
+                    return jsonify({'message': 'Token is invalid!'}), 401
+                
+                user_role = payload.get('role')
+                if user_role != required_role:
+                    return jsonify({'message': 'You are not authorized to access this route!'}), 403
+            except Exception as e:
+                return jsonify({'message': 'Token is invalid!'}), 401
+
+            return f(*args, **kwargs)
+        return decorated_function
+    return decorator
+
