89ae63794ab08006e066406ae9acbc16f9135448
omobolaji001 <omobolajiolayiwola11@gmail.com>
2024-09-08 19:36:49 +0100

    Refractor DB model for improved performance and readability

--- models/engine/db.py
+++ models/engine/db.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
-"""Defines the Database connection
-"""
+""" Database Connection """
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, scoped_session
 from sqlalchemy.orm.session import Session
 from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.exc import InvalidRequestError
-
+from dotenv import load_dotenv
+from os import getenv
+from models.base import Base
 from models.user import User
 from models.product import Product
 from models.order import Order
@@ -15,45 +15,46 @@ from models.cart import Cart
 from models.category import Category
 from models.order_item import OrderItem
 from models.cart_item import CartItem
+from models.merchant import Merchant
+from models.customer import Customer
+from models.payment import Payment
 
-
-TRADEHUB_DB_USER = getenv("TRADEHUB_DB_USER")
-TRADEHUB_DB_PWD = getenv("TRADEHUB_DB_PASSWORD")
-TRADEHUB_DB_HOST = getenv("TRADEHUB_DB_HOST")
-TRADEHUB_DB_PORT = getenv("TRADEHUB_DB_PORT")
-TRADEHUB_DB = getenv("TRADEHUB_DB")
-
-Base = declarative_base()
+load_dotenv()
 classes = {
-            "User": User, "Product": Product,
-            "Category": Caegory, "Order": Order,
-            "Cart": Cart, "OrderItem": OrderItem,
-            "CartItem": CartItem
+    "User": User, "Product": Product, "Customer": Customer,
+    "Category": Category, "Order": Order, "Payment": Payment,
+    "Cart": Cart, "OrderItem": OrderItem,
+    "CartItem": CartItem, "Merchant": Merchant
 }
 
 
 class DB:
-    """Represents the Database class
-    """
+    """Represents the Database class"""
 
     def __init__(self):
-        """Initialize a new DB instance
-        """
+        """Initialize a new DB instance"""
+        TRADEHUB_DB_USER = getenv("TRADEHUB_DB_USER")
+        TRADEHUB_DB_PWD = getenv("TRADEHUB_DB_PWD")
+        TRADEHUB_DB_HOST = getenv("TRADEHUB_DB_HOST")
+        TRADEHUB_DB_PORT = getenv("TRADEHUB_DB_PORT")
+        TRADEHUB_DB = getenv("TRADEHUB_DB")
+
         try:
-            self._engine = create_engine("postgresql://{}:{}@{}:{}/{}".format(
-                                         TRADEHUB_DB_USER, TRADEHUB_DB_PWD,
-                                         TRADEHUB_DB_HOST, TRADEHUB_DB_PORT,
-                                         TRADEHUB_DB))
+            self._engine = create_engine("postgresql+psycopg2://{}:{}@{}:{}/{}"
+                                         .format(TRADEHUB_DB_USER,
+                                                 TRADEHUB_DB_PWD,
+                                                 TRADEHUB_DB_HOST,
+                                                 TRADEHUB_DB_PORT,
+                                                 TRADEHUB_DB))
         except Exception as e:
-            print("Error while connecting to the Database": e)
+            print("Error while connecting to the Database", e)
 
         Base.metadata.create_all(self._engine)
         self.__session = None
 
     @property
     def _session(self):
-        """Represents a Memoized session object
-        """
+        """Represents a Memoized session object"""
         if self.__session is None:
             session_factory = sessionmaker(bind=self._engine)
             DBSession = scoped_session(session_factory)
@@ -61,50 +62,64 @@ class DB:
         return self.__session
 
     def all(self, cls=None):
-        """Retrieves all objects of a class
-        """
+        """Retrieves all objects of a class"""
         new = {}
         for clss in classes:
-            if cls is None or cls is classes[clss] of cls is clss:
+            if cls is None or cls is classes[clss] or cls == clss:
                 objs = self._session.query(classes[clss]).all()
                 for obj in objs:
-                    key = obj.__class__.__name__ + '.' + obj.id
+                    key = obj.__class__.__name__ + '.' + str(obj.id)
                     new[key] = obj
         return new
 
     def new(self, obj):
-        """Adds the object to the current database session
-        """
+        """Adds the object to the current database session"""
         self._session.add(obj)
 
     def save(self):
-        """Commits all changes to the database
-        """
+        """Commits all changes to the database"""
         self._session.commit()
 
     def delete(self, obj=None):
-        """Deletes obj from the current database session
-        """
+        """Deletes obj from the current database session"""
         if obj is not None:
             self._session.delete(obj)
 
     def get(self, cls, id):
-        """Retrieves an object from the database by id
-        """
+        """Retrieves an object from the database by id"""
         if cls is not None and id is not None:
-            obj = self._session.query(cls).get(id)
+            obj = self._session.get(cls, id)
             return obj
         return None
 
-    def get_by_email(self, cls, email):
-        """Retrieves object from the database by email
-        """
-        if cls is not None and email is not None:
-            obj = self._session.query(cls).filter_by(email=email).first()
-            return obj
-        return None
+    def add_user(self, email: str, hashed_password: str) -> User:
+        """Adds new user to database"""
+        user = User(email=email, hashed_password=hashed_password)
+        self._session.add(user)
+        self.save()
+        return user
+
+    def find_user_by(self, **kwargs) -> User:
+        """Finds user by key-word argument"""
+        try:
+            return self._session.query(User).filter_by(**kwargs).one()
+        except NoResultFound:
+            raise NoResultFound
+
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """Updates user"""
+        try:
+            user = self.find_user_by(id=user_id)
+        except NoResultFound:
+            raise ValueError("User not found")
+
+        for key, value in kwargs.items():
+            if hasattr(user, key):
+                setattr(user, key, value)
+            else:
+                raise ValueError(f"Attribute {key} not found on User")
+        self.save()
 
     def close(self):
-        """Closes the current DB session
-        """
+        """Closes the current DB session"""
         self._session.remove()
