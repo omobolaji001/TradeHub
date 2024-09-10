552620d9178a11145a65884498aab08d5601b24a
omobolaji001 <omobolajiolayiwola11@gmail.com>
2024-09-10 19:54:30 +0100

    Modified Merchant class to include to_dict()

--- models/merchant.py
+++ models/merchant.py
@@ -14,7 +14,18 @@ class Merchant(Base):
     id = Column(Integer, primary_key=True, autoincrement=True)
     user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
     business_name = Column(String(250), unique=True, nullable=False)
-    business_description = Column(Text, unique=True, nullable=False)
+    business_description = Column(Text, nullable=False)
     created_at = Column(DateTime, default=datetime.utcnow)
     updated_at = Column(DateTime, default=datetime.utcnow,
                         onupdate=datetime.utcnow())
+
+    def to_dict(self):
+        """ Returns dictionary of instance """
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "business_name": self.business_name,
+            "business_description": self.business_description,
+            "created_at": self.created_at,
+            "updated_at": self.updated_at
+        }
