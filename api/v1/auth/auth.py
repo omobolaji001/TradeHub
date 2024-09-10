0d617fe0b6ef44c6a9741c9f12b4cbe7a22c6391
omobolaji001 <omobolajiolayiwola11@gmail.com>
2024-09-10 19:52:08 +0100

    Modified AUTH class for better efficiency

--- api/v1/auth/auth.py
+++ api/v1/auth/auth.py
@@ -16,32 +16,12 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 JWT_BLACKLIST = set()
 
 
-def _hash_password(password: str) -> bytes:
-    """Hashes a password string and returns it in bytes form."""
-    encoded_password = password.encode('utf-8')
-    return bcrypt.hashpw(encoded_password, bcrypt.gensalt())
-
-
-def _generate_uuid() -> str:
-    """Generates a string of new uuid and returns the string."""
-    return str(uuid.uuid4())
-
 
 class Auth:
     """Auth class to interact with the authentication system."""
 
     def __init__(self):
         self._db = DB()
-
-    def register_user(self, email: str, password: str) -> User:
-        """Register a new user if they don't already exist."""
-        try:
-            self._db.find_user_by(email=email)
-        except NoResultFound:
-            hashed_password = _hash_password(password)
-            user = self._db.add_user(email, hashed_password)
-            return user
-        raise ValueError(f"User {email} already exists")
 
     def valid_login(self, email: str, password: str) -> bool:
         """Validates user credentials."""
