6a46edd2daef07695854d6ad08d8cdd86a502b12
omobolaji001 <omobolajiolayiwola11@gmail.com>
2024-09-19 16:49:36 +0100

    Add cascade on delete for user_id foreignkey

--- models/customer.py
+++ models/customer.py
@@ -10,6 +10,17 @@ class Customer(Base):
     __tablename__ = 'customers'
 
     id = Column(Integer, primary_key=True, autoincrement=True)
-    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
-    phone_number = Column(String(14), unique=True, nullable=False)
+    user_id = Column(Integer, ForeignKey('users.id', ondelete='CASCADE'),
+                     nullable=False)
     created_at = Column(DateTime, default=datetime.utcnow)
+    updated_at = Column(DateTime, default=datetime.utcnow,
+                        onupdate=datetime.utcnow())
+
+    def to_dict(self):
+        """ Returns User data in dictionary format """
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "created_at": self.created_at,
+            "updated_at": self.updated_at
+        }
