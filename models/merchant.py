086d733cbc982773ce5d4a0dd69dff69f9546abc
omobolaji001 <omobolajiolayiwola11@gmail.com>
2024-09-19 16:48:16 +0100

    Enable cascading delete for user_id in database schema

--- models/merchant.py
+++ models/merchant.py
@@ -12,7 +12,8 @@ class Merchant(Base):
     __tablename__ = 'merchants'
 
     id = Column(Integer, primary_key=True, autoincrement=True)
-    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+    user_id = Column(Integer, ForeignKey('users.id', ondelete='CASCADE'),
+                     nullable=False)
     business_name = Column(String(250), unique=True, nullable=False)
     business_description = Column(Text, nullable=False)
     created_at = Column(DateTime, default=datetime.utcnow)
