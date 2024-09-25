12dc10f1f5cf7aa725a0a6c5e186818426d24a4e
omobolaji001 <omobolajiolayiwola11@gmail.com>
2024-09-25 11:43:17 +0100

    Modified Order to include to_dict()

--- models/order.py
+++ models/order.py
@@ -2,7 +2,10 @@
 """Defines the Order model
 """
 from models.base import Base
-from sqlalchemy import Column, Integer, DateTime, Numeric, String, ForeignKey
+from sqlalchemy import (
+    Column, Integer, DateTime, Numeric,
+    String, ForeignKey, relationship
+)
 from datetime import datetime
 
 
@@ -12,10 +15,26 @@ class Order(Base):
     __tablename__ = 'orders'
 
     id = Column(Integer, primary_key=True, autoincrement=True)
-    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+    customer_id = Column(Integer, ForeignKey('customers.id'), nullable=False)
     order_date = Column(DateTime, nullable=False, default=datetime.utcnow)
-    total_amount = Column(Numeric(12, 2), nullable=False)
+    total_amount = Column(Numeric(12, 2), nullable=False, default=0.00)
+    shipping_address = Column(String(300), nullable=False)
     status = Column(String(30), default='pending')
     created_at = Column(DateTime, default=datetime.utcnow)
     updated_at = Column(DateTime, default=datetime.utcnow,
                         onupdate=datetime.utcnow())
+
+    items = relationship("OrderItem", backref="order")
+
+    def to_dict(self):
+        """ Returns a dictionary representation of the object """
+        return {
+            "id": self.id,
+            "user_id": self.id,
+            "order_date": self.order_date,
+            "total_amount": self.total_amount,
+            "shipping_address": self.shipping_address,
+            "status": self.status,
+            "created_at": self.created_at,
+            "updated_at": self.updated_at
+        }
