19ae5c8b881b2925ea423cf7ce33576d91bfdf85
omobolaji001 <omobolajiolayiwola11@gmail.com>
2024-09-25 11:43:58 +0100

    Modified OrderItem to include to_dict()

--- models/order_item.py
+++ models/order_item.py
@@ -15,7 +15,19 @@ class OrderItem(Base):
     order_id = Column(Integer, ForeignKey('orders.id'), nullable=False)
     product_id = Column(Integer, ForeignKey('products.id'), nullable=False)
     quantity = Column(Integer, nullable=False)
-    price = Column(Numeric(precision=10, scale=2), nullable=False)
+    price_per_item = Column(Numeric(precision=10, scale=2), nullable=False)
     created_at = Column(DateTime, default=datetime.utcnow)
     updated_at = Column(DateTime, default=datetime.utcnow,
                         onupdate=datetime.utcnow())
+
+    def to_dict(self):
+        """ Dictionary representation of the Item """
+        return {
+            "id": self.id,
+            "order_id": self.order_id,
+            "product_id": self.product_id,
+            "quantity": self.quantity,
+            "price_per_item": self.price_per_item,
+            "created_at": self.created_at,
+            "updated_at": self.updated_at
+        }
