49dbf7b2cdfc88d1dcc50dd598446088881ba9f1
omobolaji001 <omobolajiolayiwola11@gmail.com>
2024-09-25 11:45:26 +0100

    Redesigned Product for better readabilty

--- models/product.py
+++ models/product.py
@@ -15,10 +15,25 @@ class Product(Base):
     merchant_id = Column(Integer, ForeignKey('merchants.id'), nullable=False)
     name = Column(String(60), nullable=False)
     description = Column(String(60))
-    price = Column(Numeric(precision=10, scale=2), nullable=False)
+    price = Column(Numeric(precision=10, scale=2), nullable=False,
+                   default=0.00)
     image_url = Column(String(30), nullable=False, default='default.jpg')
     category_id = Column(Integer, ForeignKey('categories.id'),
                          nullable=False)
     created_at = Column(DateTime, default=datetime.utcnow)
     updated_at = Column(DateTime, default=datetime.utcnow,
                         onupdate=datetime.utcnow())
+
+    def to_dict(self):
+        """ Dictionary representation of object """
+        return {
+            "id": self.id,
+            "merchant_id": self.merchant_id,
+            "category_id": self.category_id,
+            "name": self.name,
+            "description": self.description,
+            "price": self.price,
+            "image_url": self.image_url,
+            "created_at": self.created_at,
+            "updated_at": self.updated_at
+        }
