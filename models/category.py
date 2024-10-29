8e367e584e15b7e9b718f4e80536b140525434a9
omobolaji001 <omobolajiolayiwola11@gmail.com>
2024-10-29 19:25:36 +0100

    Modified Category model

--- models/category.py
+++ models/category.py
@@ -1,30 +1,21 @@
 #!/usr/bin/env python3
 """Defines the Category model
 """
-from models.base import Base
-from sqlalchemy import Column, Integer, String, DateTime
+from models.base import Base, BaseModel
+from sqlalchemy import Column, String, Text
 from sqlalchemy.orm import relationship
 from datetime import datetime
 
 
-class Category(Base):
+class Category(BaseModel, Base):
     """Represents a Category
     """
     __tablename__ = 'categories'
 
-    id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String(30), nullable=False)
-    created_at = Column(DateTime, default=datetime.utcnow)
-    updated_at = Column(DateTime, default=datetime.utcnow,
-                        onupdate=datetime.utcnow())
-
+    description = Column(Text, nullable=True)
     products = relationship("Product", backref="category")
 
-    def to_dict(self):
-        """ Dictionary representation """
-        return {
-            "id": self.id,
-            "name": self.name,
-            "created_at": self.created_at,
-            "updated_at": self.updated_at
-        }
+    def __init__(*args, **kwargs):
+        """ Initialize category instance """
+        super().__init__(*args, **kwargs)
