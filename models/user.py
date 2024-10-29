fe98423c6a059234c01d8a0c403794fa7d24efde
omobolaji001 <omobolajiolayiwola11@gmail.com>
2024-10-29 19:07:12 +0100

    Modified the User model

--- models/user.py
+++ models/user.py
@@ -1,46 +1,33 @@
 #!/usr/bin/env python3
 """Defines the User class"""
-from models.base import Base
-from sqlalchemy import Column, Integer, String, Boolean, DateTime
+from models.base import Base, BaseModel
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from datetime import datetime
 
 
-class User(Base):
+class User(BaseModel, Base):
     """Represents a User
     """
     __tablename__ = 'users'
 
-    id = Column(Integer, primary_key=True, autoincrement=True)
+
     firstname = Column(String(30), nullable=False)
     lastname = Column(String(30), nullable=False)
     email = Column(String(60), unique=True, nullable=False)
     phone_number = Column(String(14), unique=True, nullable=False)
     username = Column(String(30), unique=True, nullable=False)
-    address = Column(String(600), nullable=False)
-    hashed_password = Column(String(120), nullable=False)
+    password = Column(String(120), nullable=False)
     reset_token = Column(String(250), nullable=True)
-    role = Column(String(100), nullable=False, default='Customer')
-    created_at = Column(DateTime, default=datetime.utcnow)
-    updated_at = Column(DateTime, default=datetime.utcnow,
-                        onupdate=datetime.utcnow())
+    address = Column(String(500))
+    role = Column(String(20), nullable=False, default='customer')
 
-    merchant = relationship('Merchant', backref='user', cascade='all, delete',
-                            uselist=False, passive_deletes=True)
-    customer = relationship('Customer', backref='user', cascade='all, delete',
-                            uselist=False, passive_deletes=True)
 
-    def to_dict(self):
-        """ Returns the instance in dictionary form """
-        return {
-            "id": self.id,
-            "firstname": self.firstname,
-            "lastname": self.lastname,
-            "username": self.username,
-            "email": self.email,
-            "phone_number": self.phone_number,
-            "address": self.address,
-            "role": self.role,
-            "created_at": self.created_at,
-            "updated_at": self.updated_at
-        }
+    orders = relationship("Order", backref="customer",
+                          cascade="all, delete-orphan")
+    cart = relationship("Cart", backref="customer",
+                        cascade="all, delete-orphan")
+    businesses = relationship("Business", backref="merchant")
+
+    def __init__(self, *args, **kwargs):
+        """ Initialize the User instance """
+        super().__init__(*args, **kwargs)
