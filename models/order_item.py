32ea15335ce584dd2bdef9871afced367e4e1eb0
omobolaji001 <omobolajiolayiwola11@gmail.com>
2024-10-29 19:23:05 +0100

    Modified OrderItem

--- models/order_item.py
+++ models/order_item.py
@@ -1,33 +1,21 @@
 #!/usr/bin/env python3
 """Defines Order item model
 """
-from models.base import Base
-from sqlalchemy import Column, Integer, Numeric, ForeignKey, DateTime
+from models.base import Base, BaseModel
+from sqlalchemy import Column, Integer, String, Float, ForeignKey
 from datetime import datetime
 
 
-class OrderItem(Base):
+class OrderItem(BaseModel, Base):
     """Represents an Order item
     """
     __tablename__ = 'order_items'
 
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    order_id = Column(Integer, ForeignKey('orders.id'), nullable=False)
-    product_id = Column(Integer, ForeignKey('products.id'), nullable=False)
+    order_id = Column(String(60), ForeignKey('orders.id'), nullable=False)
+    product_id = Column(String(60), ForeignKey('products.id'), nullable=False)
     quantity = Column(Integer, nullable=False)
-    price_per_item = Column(Numeric(precision=10, scale=2), nullable=False)
-    created_at = Column(DateTime, default=datetime.utcnow)
-    updated_at = Column(DateTime, default=datetime.utcnow,
-                        onupdate=datetime.utcnow())
+    price = Column(Float, nullable=False, default=0.0)
 
-    def to_dict(self):
-        """ Dictionary representation of the Item """
-        return {
-            "id": self.id,
-            "order_id": self.order_id,
-            "product_id": self.product_id,
-            "quantity": self.quantity,
-            "price_per_item": self.price_per_item,
-            "created_at": self.created_at,
-            "updated_at": self.updated_at
-        }
+    def __init__(self, *args, **kwargs):
+        """ Initialize order_item instance """
+        super().__init__(*args, **kwargs)
