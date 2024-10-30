cdbcccac9a5714d3bca1e70d83d44e8b8874ae20
omobolaji001 <omobolajiolayiwola11@gmail.com>
2024-10-30 09:09:51 +0100

    Modified routes that handle product functionalities

--- api/v1/views/products.py
+++ api/v1/views/products.py
@@ -1,22 +1,19 @@
 #!/usr/bin/env python3
 """ Routes for Product functionalities """
 from models.product import Product
-from models.merchant import Merchant
-from models.engine.db import DB
+from models import storage
 from flask import request, abort, jsonify
-from api.views import app_views
-from api.v1.auth.utils import (
-    authorize, token_required
-)
-
-db = DB()
+from api.v1.views import app_views
+from api.v1.auth.utils import authorize
+from flask_jwt_extended import get_jwt_identity, jwt_required
 
 
-@app_views('/products', methods=['POST'], strict_slashes=False)
-@token_required
+@app_views.route('/products', methods=['POST'], strict_slashes=False)
+@jwt_required()
 def register_product():
     """ Creates new product """
-    user_id = g.user_id
+    user_id = get_jwt_identity()
+
     data = request.get_json()
 
     required_fields = ["name", "description", "price", "category_id"]
@@ -26,54 +23,42 @@ def register_product():
     if missing:
         return jsonify({"error": f"Missing fields: {', '.join(missing)}"}), 400
 
-    merchant = Merchant.query.filter_by(user_id=user_id).first()
-    if merchant:
-        merchant_id = merchant.id
-
     try:
         # register new product
-        new_product = Product(merchant_id=merchant_id, name=name,
-                              category_id=category_id,
-                              description=description, price=price)
-        db.new(new_product)
-        db.save()
+        new_product = Product(user_id=user_id, **data)
+        new_product.save()
 
     except Exception:
         return jsonify({"error": "Error while registering product!"}), 500
 
 
-@app_views('/products', methods=['GET'], strict_slashes=False)
-@token_required
+@app_views.route('/products', methods=['GET'], strict_slashes=False)
 def get_all_products():
     """ Retrieves all orders of a customer """
     try:
-        products = db.all(Product)
-        return jsonify([product.to_dict() for product in products]), 200
+        products = storage.all(Product)
+        return jsonify([prdt.to_dict() for prdt in products.values()]), 200
     except Exception:
         abort(404)
 
 
-@app_views('/products/<int:product_id>', methods=['GET'], strict_slashes=False)
-@token_required
+@app_views.route('/products/<product_id>', methods=['GET'], strict_slashes=False)
 def get_product(product_id):
     """ Retrieves a specific product """
     try:
-        product = Product.query.filter_by(id=product_id).first()
+        product = storage.get_by(Product, id=product_id)
         return jsonify(product.to_dict()), 200
     except Exception:
         abort(404)
 
 
-@app_views('/products/<int:product_id>', methods=['PUT'], strict_slashes=False)
-@token_required
+@app_views.route('/products/<product_id>', methods=['PUT'], strict_slashes=False)
+@jwt_required()
 def update_product(product_id):
     """ Update order """
-    user_id = g.user_id
-    merchant = Merchant.query.filter_by(user_id=user_id).first()
-    merchant_id = merchant.id
+    user_id = get_jwt_identity()
 
-    product = Product.query.filter_by(id=product_id,
-                                      merchant_id=merchant_id).first()
+    product = storage.get_by(Product, id=product_id, user_id=user_id)
 
     if not product:
         abort(404)
@@ -83,13 +68,13 @@ def update_product(product_id):
     if not data:
         abort(400, description="Not a valid JSON")
 
-    ignore = ['created_at', 'updated_at', 'id', 'merchant_id', 'name']
+    ignore = ['created_at', 'updated_at', 'id', 'user_id']
 
     try:
         for key, value in data.items():
             if key not in ignore:
                 setattr(product, key, value)
-        db.save()
+        product.save()
         
         return jsonify({
             "message": "Product updated successfully",
@@ -99,19 +84,17 @@ def update_product(product_id):
         abort(500)
 
 
-@app_views('/products/<int:product_id>', methods=['DELETE'], strict_slashes=False)
-@token_required
+@app_views.route('/products/<product_id>', methods=['DELETE'], strict_slashes=False)
+@jwt_required()
 def delete_product(product_id):
     """ Deletes a product from the database """
-    user_id = g.user_id
-    merchant = Merchant.query.filter_by(user_id=user_id).first()
-    merchant_id = merchant.id
+    user_id = get_jwt_identity()
 
     try:
-        product = Product.query.filter_by(id=product_id,
-                                      merchant_id=merchant_id).first()
-        db.delete(product)
-        db.save()
+        product = storage.get_by(Product, id=product_id, user_id=user_id)
+        storage.delete(product)
+        storage.save()
+
         return jsonify({"message": "Product deleted successfully"}), 200
     except Exception:
         abort(404)
