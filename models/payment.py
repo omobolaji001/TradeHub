11397c5c20b6cbefd859c52dcab3071eed4d69c1
omobolaji001 <omobolajiolayiwola11@gmail.com>
2024-10-30 09:03:37 +0100

    Modified Payment class

--- models/payment.py
+++ models/payment.py
@@ -1,22 +1,22 @@
 #!/usr/bin/env python3
 """Defines the Payment model
 """
-from models.base import Base
-from sqlalchemy import Column, String, Integer, Numeric, ForeignKey, DateTime
+from models.base import Base, BaseModel
+from sqlalchemy import Column, String, Numeric, ForeignKey
 from datetime import datetime
 
 
-class Payment(Base):
+class Payment(BaseModel, Base):
     """Payment class
     """
 
     __tablename__ = 'payments'
 
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    order_id = Column(Integer, ForeignKey('orders.id'), nullable=False)
+    order_id = Column(String(60), ForeignKey('orders.id'), nullable=False)
     amount = Column(Numeric(12, 2), nullable=False)
     payment_method = Column(String(50))
     payment_status = Column(String(50), default='pending')
-    created_at = Column(DateTime, default=datetime.utcnow)
-    updated_at = Column(DateTime, default=datetime.utcnow,
-                        onupdate=datetime.utcnow())
+
+    def __init__(self, *args, **kwargs):
+        """ Initialize paymnet instances """
+        super().__init__(*args, **kwargs)
