a9d393c55bc6766ec28b8122c63693e334548bc9
omobolaji001 <omobolajiolayiwola11@gmail.com>
2024-10-31 17:04:43 +0100

    Test case for User class

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Test BaseModel for expected behavior and documentation"""
+"""Test User for expected behavior and documentation"""
 from datetime import datetime
 import inspect
 import models
